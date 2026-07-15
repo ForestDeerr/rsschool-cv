702a422367d80cb67ff6a3c948eaa10110970807
EgorGer <QwertiQQQ@gmal.com>
2026-07-15 16:47:16 +0300

    feat: add header

---
+++ src/components/header/header.js
@@ -0,0 +1,36 @@
+function createHeader(lang) {
+  const header = document.createElement("header");
+
+  const logo = document.createElement("div");
+  logo.className = "logo";
+
+  const logoImg = document.createElement("div");
+  logoImg.className = "logo-img";
+
+  const img = document.createElement("img");
+  img.className = "footer-img";
+  img.src = "elk.png";
+  img.alt = "Моё фото";
+  img.width = 200;
+  img.height = 200;
+
+  const logoTitle = document.createElement("div");
+  logoTitle.className = "logo-title";
+
+  const h1 = document.createElement("h1");
+  h1.className = "rgb";
+  h1.textContent = lang.headerName;
+
+  const h2 = document.createElement("h2");
+  h2.className = "rgb";
+  h2.textContent = lang.headerPosition;
+
+  logoImg.append(img);
+  logoTitle.append(h1, h2);
+  logo.append(logoImg, logoTitle);
+  header.append(logo);
+
+  return header;
+}
+
+export { createHeader };
