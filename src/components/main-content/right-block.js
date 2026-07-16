fe9d0eda72999ca352aaaf7641e6bce90b009f29
EgorGer <QwertiQQQ@gmal.com>
2026-07-16 12:32:29 +0300

    feat: add right block

---
+++ src/components/main-content/right-block.js
@@ -0,0 +1,10 @@
+function createRightBlock(lang) {
+  const rightBlock = document.createElement("div");
+  rightBlock.className = "right";
+
+  rightBlock.append();
+
+  return rightBlock;
+}
+
+export { createRightBlock };
