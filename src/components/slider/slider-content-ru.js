0171f32451ec9687e6fda20da9446428725e33b8
EgorGer <QwertiQQQ@gmal.com>
2026-08-16 10:18:20 +0300

    fix: update info in slider

--- src/components/slider/slider-content-ru.js
+++ src/components/slider/slider-content-ru.js
@@ -1,35 +1,46 @@
 const sliderContentRU = [
   {
-    title: "Async Race",
-    image: "./src/components/slider/assets/image.png",
-    description: ["TypeScript", "REST API", "SPA", "Vite", "CSS"],
-    info: "Одностраничное приложение, имитирующее гоночный трек. Пользователь может создавать автомобили, редактировать их, запускать гонки и определять победителя. Проект демонстрирует работу с REST API, асинхронными запросами, анимациями и архитектурой приложения на TypeScript.",
-    github: "https://github.com/yourname/async-race",
-    demo: "https://yourname.github.io/async-race/",
-  },
-  {
-    title: "News API",
-    image: "./src/components/slider/assets/image.png",
-    description: ["JavaScript", "REST API", "Fetch API", "Адаптивная верстка"],
-    info: "Приложение для просмотра новостей с использованием открытого API. Реализованы поиск, фильтрация по категориям, пагинация и адаптивный интерфейс. Проект демонстрирует работу с внешними сервисами и обработку асинхронных данных.",
-    github: "https://github.com/yourname/news-api",
-    demo: "https://yourname.github.io/news-api/",
-  },
-  {
-    title: "Nonograms",
-    image: "./src/components/slider/assets/image.png",
-    description: ["TypeScript", "Игра", "Алгоритмы", "LocalStorage"],
-    info: "Реализация логической игры «Японские кроссворды». Поддерживаются различные уровни сложности, сохранение прогресса, таймер, система подсказок и проверка решения. Основной упор сделан на алгоритмы и управление состоянием приложения.",
-    github: "https://github.com/yourname/nonograms",
-    demo: "https://yourname.github.io/nonograms/",
-  },
-  {
     title: "Water Tracker",
     image: "./src/components/slider/assets/image.png",
-    description: ["TypeScript", "Firebase", "Realtime Database", "Авторизация"],
-    info: "Веб-приложение для учета потребления питьевой воды сотрудниками. Реализованы регистрация пользователей, авторизация, синхронизация данных в реальном времени, расчет баланса и современный адаптивный интерфейс.",
-    github: "https://github.com/yourname/water-tracker",
-    demo: "https://yourname.github.io/water-tracker/",
+    description: ["TypeScript", "Firebase", "Vite", "Canvas"],
+    info: "Рабочее web-приложение для учёта потребления воды, разработанное и используемое на предприятии. Приложение работает с реальными данными и использует Firebase Realtime Database для хранения и синхронизации информации. В проект внедрена демонстрационная игра, реализованная с использованием HTML Canvas.",
+    github: "https://github.com/ForestDeerr/water-accounting",
+    demo: "https://forestdeerr.github.io/water-accounting/",
+  },
+  {
+    title: "Японские кроссворды",
+    image: "./src/components/slider/assets/image1.png",
+    description: ["TypeScript", "LocalStorage", "Игра"],
+    info: "Браузерная игра-головоломка «Японские кроссворды» с несколькими уровнями сложности. Проект включает игровую логику, интерактивное взаимодействие с пользователем, проверку решения и адаптивный интерфейс.",
+    github:
+      "https://github.com/rolling-scopes-school/forestdeerr-JSFE2024Q4/tree/gh-pages/nonograms",
+    demo: "https://rolling-scopes-school.github.io/forestdeerr-JSFE2024Q4/nonograms/",
+  },
+  {
+    title: "Асинхронная гонка",
+    image: "./src/components/slider/assets/image2.png",
+    description: ["TypeScript", "HTML", "CSS", "REST API"],
+    info: "Одностраничное web-приложение с гоночной игрой, в которой пользователи могут создавать автомобили, управлять ими и участвовать в заездах. Проект работает с REST API и включает асинхронное управление несколькими автомобилями и обработку результатов гонок.",
+    github:
+      "https://github.com/rolling-scopes-school/forestdeerr-JSFE2024Q4/tree/async-race/async-race",
+    demo: "",
+  },
+  {
+    title: "Не Бойцовский клуб",
+    image: "./src/components/slider/assets/image3.png",
+    description: ["JavaScript", "LocalStorage", "HTML", "CSS", "Game"],
+    info: "Одностраничное web-приложение, разработанное в рамках обучения в RS School. Проект представляет собой интерактивную игру с логикой боя и динамическим взаимодействием с пользователем. Реализованы работа с DOM, обработка событий, управление состоянием игры и динамическое обновление интерфейса.",
+    github: "https://github.com/ForestDeerr/not-fight-club",
+    demo: "https://forestdeerr.github.io/not-fight-club/",
+  },
+  {
+    title: "Виртуальный музыкальный набор",
+    image: "./src/components/slider/assets/image4.png",
+    description: ["JavaScript", "HTML", "CSS", "Web Audio API"],
+    info: "Интерактивное web-приложение, имитирующее виртуальный музыкальный инструмент. Пользователь может воспроизводить звуки с помощью клавиатуры и взаимодействия с интерфейсом. В проекте реализованы обработка событий, работа с DOM, воспроизведение аудио и адаптивный интерфейс.",
+    github:
+      "https://github.com/rolling-scopes-school/forestdeerr-JSFE2025Q3/tree/gh-pages/virtual-music-kit",
+    demo: "https://rolling-scopes-school.github.io/forestdeerr-JSFE2025Q3/virtual-music-kit/",
   },
 ];
 
