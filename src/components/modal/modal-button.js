8e27a6db02464d83381b80857a2bdde2f11f3b3c
EgorGer <QwertiQQQ@gmal.com>
2026-08-21 14:48:49 +0300

    refactor: info button

--- src/components/modal/modal-button.js
+++ src/components/modal/modal-button.js
@@ -3,18 +3,37 @@ import { createAboutContent } from "./create-about-content.js";
 import { createModal } from "./create-modal.js";
 
 function createModalBtn(lang) {
-  const button = createButton({
-    className: `lang-btn infoPng`,
-    onClick: () => {
-      createModal(
-        {
-          title: lang.modalContent.title,
-          content: lang.modalContent.content,
-        },
-        lang,
-      );
-    },
+  const button = document.createElement("button");
+  button.className = "info-btn";
+
+  const text = document.createElement("div");
+  text.className = "info-btn-text";
+  text.textContent = "i";
+
+  button.append(text);
+
+  button.addEventListener("click", () => {
+    createModal(
+      {
+        title: lang.modalContent.title,
+        content: lang.modalContent.content,
+      },
+      lang,
+    );
   });
+
+  // const button = createButton({
+  //   className: `lang-btn infoPng`,
+  //   onClick: () => {
+  //     createModal(
+  //       {
+  //         title: lang.modalContent.title,
+  //         content: lang.modalContent.content,
+  //       },
+  //       lang,
+  //     );
+  //   },
+  // });
 
   return button;
 }
